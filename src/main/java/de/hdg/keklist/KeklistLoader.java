2863d04952dcba55cf071b75bb421bcca36247ca
SageSphinx63920 <sage@sagesphinx63920.dev>
2023-04-07 21:52:53 +0200

    Updates and less plugin file size

---
+++ src/main/java/de/hdg/keklist/KeklistLoader.java
@@ -0,0 +1,24 @@
+package de.hdg.keklist;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.jetbrains.annotations.NotNull;
+
+public class KeklistLoader implements PluginLoader {
+    @Override
+    public void classloader(@NotNull PluginClasspathBuilder pluginClasspathBuilder) {
+        MavenLibraryResolver resolver = new MavenLibraryResolver();
+
+        resolver.addRepository(new RemoteRepository.Builder("maven", "default", "https://repo.maven.apache.org/maven2").build());
+
+        resolver.addDependency(new Dependency(new DefaultArtifact("com.squareup.okhttp3:okhttp:5.0.0-alpha.11"), "compile"));
+        resolver.addDependency(new Dependency(new DefaultArtifact("org.xerial:sqlite-jdbc:3.40.1.0"), "compile"));
+        resolver.addDependency(new Dependency(new DefaultArtifact("org.mariadb.jdbc:mariadb-java-client:3.1.2"), "compile"));
+
+        pluginClasspathBuilder.addLibrary(resolver);
+    }
+}
