b31005f1c07e3e1fc4d1260f7da89c695d925a42
SageSphinx63920 <sage@sagesphinx63920.dev>
2024-03-21 23:00:41 +0100

    Shrink jar size again
    Moved the update checker to the custom loader too

--- src/main/java/de/hdg/keklist/KeklistLoader.java
+++ src/main/java/de/hdg/keklist/KeklistLoader.java
@@ -14,11 +14,15 @@ public class KeklistLoader implements PluginLoader {
         MavenLibraryResolver resolver = new MavenLibraryResolver();
 
         resolver.addRepository(new RemoteRepository.Builder("maven", "default", "https://repo.maven.apache.org/maven2").build());
+        resolver.addRepository(new RemoteRepository.Builder("sage-snap", "default", "https://repo.sageee.xyz/snapshots").build());
+        resolver.addRepository(new RemoteRepository.Builder("sage-release", "default", "https://repo.sageee.xyz/releases").build());
+
 
         resolver.addDependency(new Dependency(new DefaultArtifact("com.squareup.okhttp3:okhttp:5.0.0-alpha.12"), "compile"));
         resolver.addDependency(new Dependency(new DefaultArtifact("org.xerial:sqlite-jdbc:3.45.2.0"), "compile"));
         resolver.addDependency(new Dependency(new DefaultArtifact("org.mariadb.jdbc:mariadb-java-client:3.3.3"), "compile"));
         resolver.addDependency(new Dependency(new DefaultArtifact("club.minnced:discord-webhooks:0.8.4"), "compile"));
+        resolver.addDependency(new Dependency(new DefaultArtifact("de.sage.util:updatechecker:1.0.3-SNAPSHOT"), "compile"));
 
         pluginClasspathBuilder.addLibrary(resolver);
     }
