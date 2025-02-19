1054df50e85a7d48faedc8b0fb934b6428c69502
SageSphinx63920 <sage@sagesphinx63920.dev>
2025-02-19 21:04:52 +0100

    Dependency updates (als always)
    Closes #167
    Closes #166

--- src/main/java/de/hdg/keklist/KeklistLoader.java
+++ src/main/java/de/hdg/keklist/KeklistLoader.java
@@ -20,7 +20,7 @@ public class KeklistLoader implements PluginLoader {
 
 
         resolver.addDependency(new Dependency(new DefaultArtifact("com.squareup.okhttp3:okhttp:5.0.0-alpha.14"), "compile"));
-        resolver.addDependency(new Dependency(new DefaultArtifact("org.xerial:sqlite-jdbc:3.49.0.0"), "compile"));
+        resolver.addDependency(new Dependency(new DefaultArtifact("org.xerial:sqlite-jdbc:3.49.1.0"), "compile"));
         resolver.addDependency(new Dependency(new DefaultArtifact("org.mariadb.jdbc:mariadb-java-client:3.5.2"), "compile"));
         resolver.addDependency(new Dependency(new DefaultArtifact("club.minnced:discord-webhooks:0.8.4"), "compile"));
         resolver.addDependency(new Dependency(new DefaultArtifact("com.github.TominoLP:2FA-API:1.3.2"), "compile"));
